8c11e85eb4d40d1fa537e75448574a14c85b8a5d
Murat Katiklilar <muratkat1903@gmail.com>
2020-02-19 21:56:33 +0200

    Proje dosyası ekle.

---
+++ StokTakip/GirisYap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokTakip
+{
+    public partial class GirisYap : Form
+    {
+        public GirisYap()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UyeOl uyeOl = new UyeOl();
+            uyeOl.ShowDialog();
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            UyeOl uyeOl = new UyeOl();
+            uyeOl.ShowDialog();
+        }
+    }
+}
