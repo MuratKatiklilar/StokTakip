f6b1cd95c5c097916cefa8fb36aa79f8e228d189
Murat Katiklilar <muratkat1903@gmail.com>
2020-02-27 01:48:58 +0200

    Update
    New Update

--- StokTakip/GirisYap.cs
+++ StokTakip/GirisYap.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace StokTakip
 {
@@ -39,6 +40,36 @@ namespace StokTakip
             this.Hide();
             UyeOl uyeOl = new UyeOl();
             uyeOl.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti;
+            SqlCommand cmd;
+            SqlDataReader dr;
+            string KullaniciAdi = textBox1.Text;
+            string Sifre = textBox2.Text;
+
+            baglanti = new SqlConnection("server=MURATPC\\SQLEXPRESS; Initial Catalog=Stok_Takip;Integrated Security=SSPI");
+            cmd = new SqlCommand();
+            baglanti.Open();
+            cmd.Connection = baglanti;
+            cmd.CommandText = "SELECT * FROM tbl_KullaniciKayit where KullaniciAdi='" + textBox1.Text + "' AND Sifre='" + textBox2.Text + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Tebrikler! Başarılı bir şekilde giriş yaptınız.");
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adını ve şifrenizi kontrol ediniz.");
+            }
+            baglanti.Close();
         }
     }
 }
--- StokTakip/UyeOl.cs
+++ StokTakip/UyeOl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace StokTakip
 {
@@ -39,6 +40,43 @@ namespace StokTakip
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection baglanti;
+            SqlCommand komut;
+            SqlDataAdapter da;
+
+            string KullaniciAdi = textBox1.Text;
+            string Sifre = textBox2.Text;
+            string TelefonNo = textBox4.Text;
+            string Mail = textBox5.Text;
+            string Meslek = comboBox1.Text;
+            string CalistigiYer = comboBox2.Text;
+            
+
+
+
+
+
+
+            baglanti = new SqlConnection("server=MURATPC\\SQLEXPRESS; Initial Catalog=Stok_Takip;Integrated Security=SSPI");
+            komut = new  SqlCommand();
+            baglanti.Open();
+            komut.Connection = baglanti;
+            komut.CommandText= "Insert into tbl_KullaniciKayit (KullaniciAdi,Sifre,TelefonNo,Mail,Meslek,CalistigiYer) values (@KullaniciAdi,@Sifre,@TelefonNo,@Mail,@Meslek,@CalistigiYer)";
+            komut.Parameters.AddWithValue("@KullaniciAdi", textBox1.Text);
+            komut.Parameters.AddWithValue("@Sifre", textBox2.Text);
+            komut.Parameters.AddWithValue("@TelefonNo", textBox4.Text);
+            komut.Parameters.AddWithValue("@Mail", textBox5.Text);
+            komut.Parameters.AddWithValue("@Meslek", comboBox1.Text);
+            komut.Parameters.AddWithValue("@CalistigiYer", comboBox2.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt İşlemi Gerçekleşti.");
+
 
         }
     }
